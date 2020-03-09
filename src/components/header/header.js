b9aa5dc2291a2e0cd928a46452f3b313fff09e68
Jack Kallend <Kallendo96@live.co.uk>
2020-03-09 11:23:59 +0000

    Initial commit

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import headerStyles from './header.module.scss'
+
+const Header = () => (
+  <header className={headerStyles.header}>
+    <h1>FEM Gatsby Introduction</h1>
+  </header>
+)
+
+export default Header
